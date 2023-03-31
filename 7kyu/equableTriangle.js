b986b21979cd4085c6978463f0dda42e0bdc3295
Mohammedodejimi@gmail.com <Mohammedodejimi@gmail.com>
2023-04-01 00:14:25 +0100

    new kata

---
+++ 7kyu/equableTriangle.js
@@ -0,0 +1,10 @@
+/*DESCRIPTION:
+A triangle is called an equable triangle if its area equals its perimeter. Return true, if it is an equable triangle, else return false. You will be provided with the length of sides of the triangle. Happy Coding!*/
+
+
+//My Solution
+
+function equableTriangle(a, b, c) {
+  let s = (a + b + c) / 2
+  return ((a + b + c) === (Math.sqrt(s * (s - a) * (s - b) * (s - c)))) ? true : false
+}
