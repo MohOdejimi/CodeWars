c6d258ba1e5aad7f3fccf7a194ff45c6a0410df4
Mohammedodejimi@gmail.com <Mohammedodejimi@gmail.com>
2023-04-17 00:03:32 +0100

    7kyu

---
+++ 7kyu/productarray.js
@@ -0,0 +1,36 @@
+/* Explanation:
+The first element in prod [] array 20 is the product of all array's elements except the first element
+
+The second element 12 is the product of all array's elements except the second element .
+
+productArray ({1,5,2}) ==> return {10,2,5}
+Explanation:
+The first element 10 is the product of all array's elements except the first element 1
+
+The second element 2 is the product of all array's elements except the second element 5
+
+The Third element 5 is the product of all array's elements except the Third element 2.
+
+productArray ({10,3,5,6,2}) return ==> {180,600,360,300,900}
+Explanation:
+The first element 180 is the product of all array's elements except the first element 10
+
+The second element 600 is the product of all array's elements except the second element 3
+
+The Third element 360 is the product of all array's elements except the third element 5
+
+The Fourth element 300 is the product of all array's elements except the fourth element 6
+
+Finally ,The Fifth element 900 is the product of all array's elements except the fifth element 2*/
+
+// My Solution 
+
+function productArray(numbers) {
+  //your code here
+  let product = []
+  for (let i = 0; i < numbers.length; i++) {
+    let result = numbers.reduce((x, y) => x * y)
+    product.push(result / numbers[i])
+  }
+  return product
+}
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 CodeWars Challenges.
+
+
 A repository filled with CodeWars challenges(Kata). Currently ranked in the top 70 percentile in the global leaderboard. I push at least 5 solutions every week.
 
 Direct Link to my codeWar profile 
-https://www.codewars.com/users/MohOdejimi/stats
 
 Everyday before going to bed I attempt a Kata exercise to sharpen my problem solving skills.
