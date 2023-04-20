4a6dc6f9d2b9991b7c5593b1eb5401ad6bb32d79
Mohammedodejimi@gmail.com <Mohammedodejimi@gmail.com>
2023-04-20 17:16:11 +0100

    7kyu

---
+++ 7kyu/summingnumberdigits.js
@@ -0,0 +1,17 @@
+/*DESCRIPTION:
+Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+For example: (Input --> Output)
+
+10 --> 1
+99 --> 18
+-32 --> 5
+Let's assume that all numbers in the input will be integer values.*/
+
+// My Solution
+
+function sumDigits(numbers) {
+  let arrOfNum = Array.of(Math.abs(numbers)).join().split('')
+  return Number(arrOfNum.reduce((x, y) => Number(x) + Number(y)))
+}
+
