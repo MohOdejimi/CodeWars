f053ea2f8b39b59addf7176b610f931a18388908
Mohammedodejimi@gmail.com <Mohammedodejimi@gmail.com>
2023-05-08 17:22:33 +0100

    7kyu

---
+++ 7kyu/alphabetically-ordered.js
@@ -0,0 +1,21 @@
+/*DESCRIPTION:
+Your task is very simple. Just write a function that takes an input string of lowercase letters and returns true/false depending on whether the string is in alphabetical order or not.
+
+Examples (input -> output)
+"kata" -> false ('a' comes after 'k')
+"ant" -> true (all characters are in alphabetical order)
+Good luck :) */
+
+
+// My Solution. 
+
+function alphabetic(s) {
+  // Yor code here
+  let copy = [...(s.split(''))]
+  let order = (copy.sort()).join('')
+  if (order === s) {
+    return true
+  } else {
+    return false
+  }
+}
