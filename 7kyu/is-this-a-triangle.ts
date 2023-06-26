8c33bba84ba2985c788fa4b6c388965b38f83995
Mohammedodejimi@gmail.com <Mohammedodejimi@gmail.com>
2023-06-26 08:42:59 +0100

    7kyu

---
+++ 7kyu/is-this-a-triangle.ts
@@ -0,0 +1,10 @@
+/* DESCRIPTION:
+Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted). */
+
+// My Solution 
+
+if (a <= 0 || b <= 0 || c <= 0) return false
+// String Inequality Theorem 
+return (a + b >= c && a + c >= b && b + c >= a) ? true : false
