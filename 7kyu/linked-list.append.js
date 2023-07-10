801a18d2ddb6cdbb437c7984b3c2d3b90bc8d2d4
Mohammedodejimi@gmail.com <Mohammedodejimi@gmail.com>
2023-07-10 21:11:35 +0100

    7kyu

---
+++ 7kyu/linked-list.append.js
@@ -0,0 +1,32 @@
+/* DESCRIPTION:
+Linked Lists - Append
+
+Write an Append() function which appends one linked list to another. The head of the resulting list should be returned.
+
+var listA = 1 -> 2 -> 3 -> null
+var listB = 4 -> 5 -> 6 -> null
+append(listA, listB) === 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null
+If both listA and listB are null/NULL/None/nil, return null/NULL/None/nil. If one list is null/NULL/None/nil and the other is not, simply return the non-null/NULL/None/nil list. */
+
+// My Solution 
+
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function append(listA, listB) {
+  // Your code goes here.
+  // Remember to return the head of the list.
+  if(!listA && !listB) return null
+  if(!listA) return listB
+  if(!listB) return listA
+  
+  let current = listA
+  
+  while(current.next!==null) current = current.next
+  
+  current.next = listB
+  
+  return listA
+}
