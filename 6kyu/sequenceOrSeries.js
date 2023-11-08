4723324086827b735602ef33d2ef82e551a3d3fd
Mohammedodejimi@gmail.com <Mohammedodejimi@gmail.com>
2023-11-08 14:23:44 +0100

    6kyu

---
+++ 6kyu/sequenceOrSeries.js
@@ -0,0 +1,25 @@
+/* DESCRIPTION:
+Have a look at the following numbers.
+
+ n | score
+---+-------
+ 1 |  50
+ 2 |  150
+ 3 |  300
+ 4 |  500
+ 5 |  750
+Can you find a pattern in it? If so, then write a function getScore(n)/get_score(n)/GetScore(n) which returns the score for any positive number n.
+
+Note Real test cases consists of 100 random cases where 1 <= n <= 10000 */
+
+
+// My Solution 
+
+function getScore(n) {
+  // do your magic here
+  let score = 0
+  for(let j = n; j >= 1; j--) {
+    score += (50 * j)
+  }
+  return score 
+}
